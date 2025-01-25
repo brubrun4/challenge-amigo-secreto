3cfe662412a8b1227fb7eab113dc23f0e460909a
Bruna Cruz <rodriguescruz.bruna@gmail.com>
2025-01-25 18:50:35 -0300

    implementação da função para adicionar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,7 +1,30 @@
 let amigos = [];
-let nomeAmigo = ("");
 
 function adicionarAmigo() {
     let nomeAmigo = document.querySelector("input").value;
+
+    // Verifica se o campo está vazio
+    if (nomeAmigo.trim() === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
     amigos.push(nomeAmigo);
+    exibirNomeNaTela();
+    limparNomeAdicionado();
+}
+
+function limparNomeAdicionado() {
+    document.querySelector("input").value = "";
+}
+
+function exibirNomeNaTela() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = ""; // Limpa a lista para evitar duplicação
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
 }
