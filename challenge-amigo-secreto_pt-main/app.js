d1acfe74a8163e683e11589e1f46d32627aae82e
Bruna Cruz <rodriguescruz.bruna@gmail.com>
2025-01-25 16:56:21 -0300

    primeiras funções

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nomeAmigo = ("");
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector("input").value;
+    amigos.push(nomeAmigo);
+}
